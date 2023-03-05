1cdcfff7e0850866614c58c4cafdea87c8294218
wevans <paul@wevans.io>
2023-03-05 12:35:37 -0500

    Health checks

---
+++ config/initializers/healthcheck.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+Healthcheck.configure do |config|
+  config.success = 200
+  config.error = 503
+  config.verbose = true
+  config.route = '/healthcheck'
+  config.method = :get
+  # -- Checks --
+   config.add_check :database,     -> { ActiveRecord::Base.connection.execute('select 1') }
+   config.add_check :migrations,   -> { ActiveRecord::Migration.check_pending! }
+end
